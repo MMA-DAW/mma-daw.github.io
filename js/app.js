9edba26bfb592cd3a297e1185b9fa4474ca3deda
DAW-GitHub <71730594+DAW-GitHub@users.noreply.github.com>
2021-03-06 21:49:14 +0100

    Corrección problemas menú

---
+++ js/app.js
@@ -0,0 +1,15 @@
+
+window.addEventListener("load", start); //evento load
+
+function start(){
+    
+    button_input.addEventListener("click", menu);
+    
+}
+
+//FUNCTION toTurn(): gira las cartas que están ocultas
+function menú(){
+    
+
+
+}
