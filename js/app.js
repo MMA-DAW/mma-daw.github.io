2f9c6447e058cb48504fec9d836df1ed62b90c63
DAW-GitHub <71730594+DAW-GitHub@users.noreply.github.com>
2021-03-07 13:49:05 +0100

    Responsive y fallos del menú

--- js/app.js
+++ js/app.js
@@ -2,14 +2,30 @@
 window.addEventListener("load", start); //evento load
 
 function start(){
+    var menu_button = document.getElementById("b-h");
     
-    button_input.addEventListener("click", menu);
+    menu_button.addEventListener("click", menu);
+
+    console.log("buton actualizado");
     
 }
 
 //FUNCTION toTurn(): gira las cartas que están ocultas
-function menú(){
+function menu(){
     
+    var nav = document.getElementById("navegacion");
 
+    console.log(nav);
+
+    nav.style.display= "flex";
+ 
+}
+
+function menuInterno(){
+    var nav = document.getElementsByClassName("segundary");
+
+    console.log(nav);
+
+    nav.style.display= "flex";
 
 }
---
+++ js/novedad.js
@@ -0,0 +1,49 @@
+var rutas = [["img/news/new-memorama.png", "img/news/new-memorama-small.png"], ["img/news/new-web.png", "img/news/new-web.png"]];
+var novedades_section = document.getElementById("news");
+var descripcion_div = document.getElementById("desc");
+var count = -1;
+
+window.addEventListener("load", start); //evento load
+
+function start(){
+
+    salida(0);
+
+    setInterval(salida,3000);
+  
+}
+
+function salida(){
+    count ++;
+    if( window.matchMedia("(min-width: 820px").matches){
+        novedades_section.style.backgroundImage = "URL(" + rutas[count][0] + ")";
+        descripcion_div.innerHTML = textos(count);
+    } else{
+        novedades_section.style.backgroundImage = "URL(" + rutas[count][1] + ")";
+        descripcion_div.innerHTML = textos(count);
+    }
+    
+
+    if(count == rutas.length - 1 ){
+        count = -1;
+    }
+
+}
+
+function textos(option){
+    var texto = "";
+
+    switch(option){
+        
+        case 0:
+            texto = "<h2>MEMORAMA</h2> <h3>¡JUEGA YA!</h3>";
+            break;
+
+        case 1:
+            texto = "<h2>YA ESTÁ AQUÍ WEB-FLEX</h2> <h3>[EN CONSTRUCCIÓN]</h3>";
+            break;
+    }
+
+    return texto;
+
+}
